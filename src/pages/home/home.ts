d33d603bfeebb8954c8672f7500bab017b12e496
Gino17 <galentro451@gmail.com>
2018-08-14 16:49:53 -0700

    added functionality to functions in home.ts, fixed addChecklist typo in home.html template

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,23 +23,91 @@ export class HomePage {
   }
 
   addChecklist(): void {
+    let prompt = this.alertCtrl.create({
+      title: 'New Checklist',
+      message: 'Enter the name of your new checklist below:',
 
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+
+        {
+          text: 'Save',
+          handler: data => {
+            let newChecklist = new ChecklistModel(data.name, []);
+            this.checklists.push(newChecklist);
+
+            newChecklist.checklistUpdates().subscribe(update => {
+              this.save();
+            });
+
+            this.save();
+          }
+        }
+      ]
+    });
+
+    prompt.present();
   }
 
   renameChecklist(checklist): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Rename Checklist',
+      message: 'Enter the new name of the checklist below:',
 
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+
+        {
+          text: 'Save',
+          handler: data => {
+            let index = this.checklists.indexOf(checklist);
+
+            if(index > -1) {
+              this.checklists[index].setTitle(data.name);
+              this.save();
+            }
+          }
+        }
+      ]
+    });
+
+    prompt.present();
   }
 
   viewChecklist(checklist): void {
-
+    this.navCtrl.push('ChecklistPage', {
+      checklist: checklist
+    });
   }
 
   removeChecklist(checklist): void {
+    let index = this.checklists.indexOf(checklist);
 
+    if(index > -1) {
+      this.checklists.splice(index, 1);
+
+      this.save();
+    }
   }
 
   save(): void {
-    
+
   }
 
 }
