244e23d6884557e32708dd8020b293803e46d054
Gino17 <galentro451@gmail.com>
2018-08-15 15:04:18 -0700

    added functionality to functions in checklist.ts, modified checklist.html template

--- src/pages/checklist/checklist.ts
+++ src/pages/checklist/checklist.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ChecklistPage page.
@@ -15,11 +15,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChecklistPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  checklist: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+
+    this.checklist = this.navParams.get('checklist');
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChecklistPage');
+  addItem(): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: 'Enter the name of the task for this checklist below:',
+
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+
+        {
+          text: 'Save',
+          handler: data => {
+            this.checklist.addItem(data.name);
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  toggleItem(item): void {
+  }
+
+  removeItem(item): void {
+    this.checklist.removeItem(item);
+  }
+
+  renameItem(item): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Rename Item',
+      message: 'Enter the new name of the task for this checklist below:',
+
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+
+        {
+          text: 'Save',
+          handler: data => {
+            this.checklist.renameItem(item, data.name);
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  uncheckItems(): void {
+    this.checklist.items.forEach((item) => {
+      if(item.checked) {
+        this.checklist.toggleItem(item);
+      }
+    });
   }
 
 }
