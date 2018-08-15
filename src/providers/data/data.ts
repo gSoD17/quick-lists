47e373a5907907a680792f13b3621f2072d95ec0
Gino17 <galentro451@gmail.com>
2018-08-15 15:28:21 -0700

    added coded functions to data provider

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class DataProvider {
 
-  constructor() {
-    console.log('Hello DataProvider Provider');
+  constructor(public storage: Storage) {
+
+  }
+
+  getData(): Promise<any> {
+    return this.storage.get('checklists');
+  }
+
+  saveData(data): void {
+    let saveData = [];
+
+    data.forEach((checklist) => {
+      saveData.push({
+        title: checklist.title,
+        items: checklist.items
+      });
+    });
+
+    let newData = JSON.stringify(saveData);
+    this.storage.set('checklists', newData)
   }
 
 }
